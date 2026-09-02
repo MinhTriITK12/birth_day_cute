8bc55bd4731dd5c8a15684f7839d59748e880eab
MinhTriITK12 <nguyendung09021971@gmail.com>
2026-09-02 18:04:39 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/birth_day_cute/',
-  plugins: [react()],
+  base: './',
+  plugins: [react(), viteSingleFile()],
 })
